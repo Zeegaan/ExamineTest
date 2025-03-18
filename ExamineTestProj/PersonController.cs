1de34fccdff19488a3d7512c3f0965be61f489f9
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2025-03-18 12:53:20 +0100

    Start person controller

---
+++ ExamineTestProj/PersonController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExamineTestProj;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class PersonController : ControllerBase
+{
+    private Person[] _people = {
+        new("Nikolaj", "Geisle", "nge@ubmraoc.dk", 29),
+        new("Nikolaj", "Aaaa", "nny@email.ciom", 15),
+    };
+    
+    [HttpGet]
+    public IActionResult All()
+    {
+        return Ok(_people);
+    }
+}
+
+public class Person(string firstName, string surName, string email, int age)
+{
+    public string FirstName { get; init; } = firstName;
+    
+    public string Surname { get; init; } = surName;
+
+    public string Email { get; init; } = email;
+
+    public int Age { get; init; } = age;
+}
